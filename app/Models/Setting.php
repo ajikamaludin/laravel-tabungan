e25724594514ef76406448299c4fcb24a83936a9
Aji Kamaludin <aji19kamaludin@gmail.com>
2023-08-28 22:00:01 +0700

    init

---
+++ app/Models/Setting.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUlids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Setting extends Model
+{
+    use HasFactory, SoftDeletes, HasUlids;
+
+    protected $fillable = [
+        'key',
+        'value',
+        'type',
+    ];
+}
